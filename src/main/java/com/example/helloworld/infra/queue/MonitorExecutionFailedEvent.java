37e0cbe9dc0be075759aa1ec8ea3f0948cea5531
Otavio <otaviomiranda@usp.br>
2026-08-05 14:27:01 -0300

    add dead letter

--- src/main/java/com/example/helloworld/infra/queue/MonitorExecutionFailedEvent.java
+++ src/main/java/com/example/helloworld/infra/queue/MonitorExecutionFailedEvent.java
@@ -1,6 +1,10 @@
 package com.example.helloworld.infra.queue;
 
 import com.example.helloworld.domain.entities.ExecutionStatus;
+import com.example.helloworld.service.IncidentService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
@@ -9,4 +13,19 @@ public record MonitorExecutionFailedEvent(
         UUID executionId,
         ExecutionStatus status
 ) {
+    @Service
+    public static class IncidentConsumer {
+        @Autowired
+        public IncidentService incidentService;
+
+        @RabbitListener(queues = RabbitMQConfig.INCIDENT_QUEUE)
+        public void consume(MonitorExecutionFailedEvent event) {
+            incidentService.createIncident(event);
+        }
+
+        @RabbitListener(queues = RabbitMQConfig.RESOLVED_INCIDENT_QUEUE)
+        public void consume(MonitorExecutionResolvedEvent event) {
+            incidentService.resolveIncident(event);
+        }
+    }
 }
