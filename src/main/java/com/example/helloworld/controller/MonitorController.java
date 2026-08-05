e7dd0e66f9da9df64b32a05eb66a14e7180e6be8
Otavio <otaviomiranda@usp.br>
2026-08-05 00:07:17 -0300

    feat: implementa monitor summary endpoint

--- src/main/java/com/example/helloworld/controller/MonitorController.java
+++ src/main/java/com/example/helloworld/controller/MonitorController.java
@@ -2,12 +2,15 @@ package com.example.helloworld.controller;
 
 import com.example.helloworld.controller.dto.CreateMonitorRequest;
 import com.example.helloworld.domain.entities.MonitorEntity;
+import com.example.helloworld.infra.repositories.dto.MonitorSummary;
 import com.example.helloworld.service.MonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClient;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/monitors")
@@ -20,5 +23,12 @@ public class MonitorController {
         var monitor = monitorService.createMonitor(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(monitor);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MonitorSummary> getMonitorById(@PathVariable UUID id) {
+       var summary = this.monitorService.findById(id);
+       return ResponseEntity.status(HttpStatus.OK)
+               .body(summary);
     }
 }
