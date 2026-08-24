fc8f14a92f6a3a795835073b4dc0901871c40560
Otavio <otaviomiranda@usp.br>
2026-08-24 11:16:59 -0300

    wip add userId  no dto

--- src/main/java/com/example/helloworld/controller/dto/CreateMonitorRequest.java
+++ src/main/java/com/example/helloworld/controller/dto/CreateMonitorRequest.java
@@ -3,8 +3,14 @@ package com.example.helloworld.controller.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
 
 public record CreateMonitorRequest(
+        @NotBlank
+        @Length( , message = "O userId deve ter 36 caracteres")
+        UUID userId,
         @NotBlank(message = "O nome é obrigatório")
         @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
         String name,
